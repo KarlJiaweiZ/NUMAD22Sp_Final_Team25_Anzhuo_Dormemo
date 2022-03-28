fa90f4d3dc7bd1630c573f07575e8b8042e6a7f3
KKKH <kkkhzjw@vip.qq.com>
2022-03-28 01:38:21 -0400

    add tabsadapter and fragments

--- app/src/main/java/com/example/numad22sp_final_team25_anzhuo_dormemo/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_final_team25_anzhuo_dormemo/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.numad22sp_final_team25_anzhuo_dormemo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private Toolbar myToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabAccessorAdapter mytabAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(toolbar);
+        //set the tool bar in Dormemo
+        myToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(myToolbar);
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        mytabAccessorAdapter = new TabAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(mytabAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
     }
 
     private void setSupportActionBar(@NonNull Toolbar toolbar) {
