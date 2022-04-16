0b52e5fe21f506ecc1fcb35dffe9f56cacb8e877
Zhongshi Luo <752828402@qq.com>
2022-04-16 00:53:14 -0400

    bill recycler view design finished

---
+++ app/src/main/java/com/example/numad22sp_final_team25_anzhuo_dormemo/bill/BillCard.java
@@ -0,0 +1,14 @@
+package com.example.numad22sp_final_team25_anzhuo_dormemo.bill;
+
+public class BillCard implements BillCardClickListener{
+
+    @Override
+    public String onBillCardClick(int position) {
+        return null;
+    }
+
+    @Override
+    public void onCheckBoxClick(int position) {
+
+    }
+}
---
+++ app/src/main/java/com/example/numad22sp_final_team25_anzhuo_dormemo/bill/BillCardClickListener.java
@@ -0,0 +1,8 @@
+package com.example.numad22sp_final_team25_anzhuo_dormemo.bill;
+
+public interface BillCardClickListener {
+
+    String onBillCardClick(int position);
+
+    void onCheckBoxClick(int position);
+}
