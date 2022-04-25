cbfa737a3391fe5e1dcd317f9de79112065c6dc4
FengqiQiao <qiao.fe@husky.neu.edu>
2022-04-25 16:44:59 -0700

    Add login precheck
    Signed-off-by: FengqiQiao <qiao.fe@husky.neu.edu>

--- app/src/main/java/com/example/numad22sp_final_team25_anzhuo_dormemo/LaunchActivity.java
+++ app/src/main/java/com/example/numad22sp_final_team25_anzhuo_dormemo/LaunchActivity.java
@@ -7,18 +7,34 @@ import android.os.Handler;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(LaunchActivity.this, MainActivity.class));
                 finish();
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                if(currentUser == null || Objects.isNull(currentUser)){
+                    SendUserToLoginActivity();
+                }
             }
         }, 1000);
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(LaunchActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
